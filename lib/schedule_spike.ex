5591d00bec8427abf53ccb83fa7cc55d61c9c03a
samhstn <shouston3@me.com>
2020-12-06 20:36:28 +0000

    mix new schedule_spike --sup

---
+++ lib/schedule_spike.ex
@@ -0,0 +1,18 @@
+defmodule ScheduleSpike do
+  @moduledoc """
+  Documentation for `ScheduleSpike`.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> ScheduleSpike.hello()
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
---
+++ test/schedule_spike_test.exs
@@ -0,0 +1,8 @@
+defmodule ScheduleSpikeTest do
+  use ExUnit.Case
+  doctest ScheduleSpike
+
+  test "greets the world" do
+    assert ScheduleSpike.hello() == :world
+  end
+end
