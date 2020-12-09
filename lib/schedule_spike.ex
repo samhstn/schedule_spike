0e5f9e42592c2d2522d072b5fb29b5fe2fab518a
samhstn <shouston3@me.com>
2020-12-09 22:34:23 +0000

    update handle_call to use update_frequency naming and use single timer reference in state as opposed to a list of timers

--- lib/schedule_spike.ex
+++ lib/schedule_spike.ex
@@ -14,41 +14,40 @@ defmodule ScheduleSpike do
   def init(frequency) do
     schedule_work(frequency)
 
-    {:ok, {frequency, []}}
+    {:ok, {frequency, nil}}
   end
 
   defp frequency_to_ms(:short), do: @short
   defp frequency_to_ms(:medium), do: @medium
   defp frequency_to_ms(:long), do: @long
 
-  def never(), do: GenServer.call(__MODULE__, {:work, :never})
-  def short(), do: GenServer.call(__MODULE__, {:work, :short})
-  def medium(), do: GenServer.call(__MODULE__, {:work, :medium})
-  def long(), do: GenServer.call(__MODULE__, {:work, :long})
+  def never(), do: GenServer.call(__MODULE__, {:update_frequency, :never})
+  def short(), do: GenServer.call(__MODULE__, {:update_frequency, :short})
+  def medium(), do: GenServer.call(__MODULE__, {:update_frequency, :medium})
+  def long(), do: GenServer.call(__MODULE__, {:update_frequency, :long})
 
-  defp update_timers(frequency, timers) do
-    Enum.each(timers, &Process.cancel_timer/1)
-
-    case frequency do
-      :never -> timers
-      _ -> [schedule_work(frequency) | Enum.filter(timers, &Process.read_timer/1)]
+  defp update_timer(frequency, timer) do
+    if timer do
+      Process.cancel_timer(timer)
     end
+
+    schedule_work(frequency)
   end
 
-  def handle_call({:work, frequency}, _from, {old_frequency, timers}) do
-    new_timers = update_timers(frequency, timers)
+  def handle_call({:update_frequency, frequency}, _from, {old_frequency, timer}) do
+    new_timer = update_timer(frequency, timer)
 
     {:reply,
-     "Updating schedule from #{old_frequency} to #{frequency}, with timers: #{inspect(new_timers)}",
-     {frequency, new_timers}}
+     "Updating schedule from #{old_frequency} to #{frequency}, with timer: #{inspect(new_timer)}",
+     {frequency, new_timer}}
   end
 
-  def handle_info({:work, frequency}, {_old_frequency, timers}) do
-    new_timers = update_timers(frequency, timers)
+  def handle_info({:work, frequency}, {_old_frequency, timer}) do
+    new_timer = update_timer(frequency, timer)
 
-    Logger.info("Work after #{frequency_to_ms(frequency)}ms, with timers #{inspect(new_timers)}")
+    Logger.info("Work after #{frequency_to_ms(frequency)}ms, with timer #{inspect(new_timer)}")
 
-    {:noreply, {frequency, new_timers}}
+    {:noreply, {frequency, new_timer}}
   end
 
   defp schedule_work(:never), do: nil
